ef1f8cff413968cf40bc180bd444dee66002ddf2
Marleen Reiding <reiding01@live.nl>
2020-04-05 12:31:22 +0200

    First Commit

---
+++ headers/navigator.php
@@ -0,0 +1,16 @@
+
+ <div class="navigator">
+<!--  Code of the navigator  -->
+		<div class="logo">
+			<a href="../index.php"><h1>Chinese Blog</h1></a>
+		</div>
+		<ul class="navigatorUl">
+			<li><a class="active" href="../index.php">Home</a></li>
+			<li><a href="#news">News</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<li><a href="#about"></a></li>
+		</ul>
+	</div>
+
+	<!-- Einde navbar -->
+
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chinese Blog</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+	<div class="container">
+<!-- 	This container wraps the whole page together -->	
+	<?php
+	include('headers/navigator.php');
+	?>
+<!-- All content  -->	
+<div class="watchContent">
+	<h2 class="contentTitle">Recent Articles:</h2>
+	<hr>
+	<!-- If there's more content show here -->
+</div>
+<!-- Closing page content  -->
+
+<!-- Include footer -->
+<?php
+include('footers/mainFooter.php');
+?>
+</div>
+
+
+</body>
+</html>
